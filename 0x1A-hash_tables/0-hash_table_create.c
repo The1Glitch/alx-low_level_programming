0d38571c885d4660dc12a5f2d7117f144bf91874
Bonolo <easytmashabela@gmail.com>
2024-02-07 15:37:05 -0800

    hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - create a has table.
+ * hash_table_create - creates a hash table.
  * @size: the size the array.
  *
  * Return: if an error occurs - NULL.
